3773feb106a98b526cf881d22b819016f88b7da5
jasperan <23caj23@gmail.com>
2024-08-26 19:28:50 +0200

    feat: added cohere script

---
+++ generate_cohere.py
@@ -0,0 +1,38 @@
+import oci
+import yaml
+
+
+with open('config.yaml', 'r') as file:
+    config_data = yaml.safe_load(file)
+
+compartment_id = config_data['compartment_id']
+
+
+CONFIG_PROFILE = config_data['config_profile']
+config = oci.config.from_file('~/.oci/config', CONFIG_PROFILE)
+
+# Service endpoint
+endpoint = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
+
+generative_ai_inference_client = oci.generative_ai_inference.GenerativeAiInferenceClient(config=config, service_endpoint=endpoint, retry_strategy=oci.retry.NoneRetryStrategy(), timeout=(10,240))
+chat_detail = oci.generative_ai_inference.models.ChatDetails()
+
+chat_request = oci.generative_ai_inference.models.CohereChatRequest()
+chat_request.message = "Hello"
+chat_request.max_tokens = 300
+chat_request.temperature = 1
+chat_request.frequency_penalty = 0
+chat_request.top_p = 0.75
+chat_request.top_k = 0
+
+
+chat_detail.serving_mode = oci.generative_ai_inference.models.OnDemandServingMode(model_id="ocid1.generativeaimodel.oc1.us-chicago-1.amaaaaaask7dceyawk6mgunzodenakhkuwxanvt6wo3jcpf72ln52dymk4wq")
+chat_detail.chat_request = chat_request
+chat_detail.compartment_id = compartment_id
+chat_response = generative_ai_inference_client.chat(chat_detail)
+# Print result
+print("**************************Chat Result**************************")
+print(vars(chat_response))
+
+# llama3: ocid1.generativeaimodel.oc1.us-chicago-1.amaaaaaask7dceyaycmwwnvu2gaqrffquofgmshlqzcdwpk727n4cykg34oa
+# cohere.command-r-plus: ocid1.generativeaimodel.oc1.us-chicago-1.amaaaaaask7dceya7ozidbukxwtun4ocm4ngco2jukoaht5mygpgr6gq2lgq
