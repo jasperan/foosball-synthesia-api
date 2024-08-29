9b6ebe271cc9f19529a0dc9263ee754233c6352c
jasperan <23caj23@gmail.com>
2024-08-29 12:17:15 +0200

    fix: quick fix

--- synthesia.py
+++ synthesia.py
@@ -5,54 +5,54 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-avatars = {
-    'brendan': 'f05b2c94-6c0c-4d71-82e5-90fd51c7eea3',
-    'brendan_v2': '209701e1-77a3-40d2-a383-1bd115f0ab0e',
-    'brendan_v3': 'b449c042-ab6c-421c-b09a-baeab47d5960',
-}
-
-url = "https://api.synthesia.io/v2/videos"
-
-# Define the endpoint URL
-
-# Define the data payload
-payload = {
-    "test": True,
-    "visibility": "private",
-    "aspectRatio": "16:9",
-    "title": "Default video title",
-    "description": "Default video description",
-    "soundtrack": "inspirational",
-    "input": [
-        {
-            "avatarSettings": {
-                "horizontalAlign": "center",
-                "scale": 1,
-                "style": "rectangular",
-                "seamless": False
-            },
-            "backgroundSettings": { "videoSettings": {
-                    "shortBackgroundContentMatchMode": "freeze",
-                    "longBackgroundContentMatchMode": "trim"
-                } },
-            "avatar": avatars['brendan_v3'], # anna_costume1_cameraA
-            "scriptText": "What is up ladies and gentleman from Oracle? This is a generation example using the test version of the API. Civility vicinity graceful is it at. Improve up at to on mention perhaps raising. Way building not get formerly her peculiar. Up uncommonly prosperous sentiments simplicity acceptance to so. Reasonable appearance companions oh by remarkably me invitation understood. Pursuit elderly ask perhaps all. Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income. The expense windows adapted sir. Wrong widen drawn ample eat off doors money. Offending belonging promotion provision an be oh consulted ourselves it. Blessing welcomed ladyship she met humoured sir breeding her. Six curiosity day assurance bed necessary. ",
-            "background": "green_screen"
-        }
-    ]
-}
-
-# Set up the headers
-headers = {
-    "Authorization": yaml.safe_load(open('config.yaml'))['authorization'],
-    "accept": "application/json",
-    "content-type": "application/json"
-}
-
-
 # Synthesia localhost endpoint listening to POST requests.
 @app.route('/synthesia', methods=['POST'])
 def handle_synthesia_request():
+
+    avatars = {
+        'brendan': 'f05b2c94-6c0c-4d71-82e5-90fd51c7eea3',
+        'brendan_v2': '209701e1-77a3-40d2-a383-1bd115f0ab0e',
+        'brendan_v3': 'b449c042-ab6c-421c-b09a-baeab47d5960',
+    }
+
+    url = "https://api.synthesia.io/v2/videos"
+
+    # Define the data payload
+    payload = {
+        "test": True,
+        "visibility": "private",
+        "aspectRatio": "16:9",
+        "title": "Default video title",
+        "description": "Default video description",
+        "soundtrack": "inspirational",
+        "input": [
+            {
+                "avatarSettings": {
+                    "horizontalAlign": "center",
+                    "scale": 1,
+                    "style": "rectangular",
+                    "seamless": False
+                },
+                "backgroundSettings": { "videoSettings": {
+                        "shortBackgroundContentMatchMode": "freeze",
+                        "longBackgroundContentMatchMode": "trim"
+                    } },
+                "avatar": avatars['brendan_v3'], # anna_costume1_cameraA
+                "scriptText": "What is up ladies and gentleman from Oracle? This is a generation example using the test version of the API. Civility vicinity graceful is it at. Improve up at to on mention perhaps raising. Way building not get formerly her peculiar. Up uncommonly prosperous sentiments simplicity acceptance to so. Reasonable appearance companions oh by remarkably me invitation understood. Pursuit elderly ask perhaps all. Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income. The expense windows adapted sir. Wrong widen drawn ample eat off doors money. Offending belonging promotion provision an be oh consulted ourselves it. Blessing welcomed ladyship she met humoured sir breeding her. Six curiosity day assurance bed necessary. ",
+                "background": "green_screen"
+            }
+        ]
+    }
+
+    # Set up the headers
+    headers = {
+        "Authorization": yaml.safe_load(open('config.yaml'))['authorization'],
+        "accept": "application/json",
+        "content-type": "application/json"
+    }
+
+
+
     data = request.json
     global payload
     if data and 'text' in data:
